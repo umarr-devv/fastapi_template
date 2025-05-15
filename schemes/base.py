bad49da8ff16dbf23e96dc0252844be439e8de13
umarr <umarr.developer@gmail.com>
2025-05-15 16:45:55 +0600

    create FileSchemem BaseScheme

---
+++ schemes/base.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict
+
+
+class BaseScheme(BaseModel):
+    id: int
+    create_at: datetime
+    update_at: datetime
+
+    model_config = ConfigDict(
+        from_attributes=True
+    )
---
+++ schemes/file.py
@@ -0,0 +1,6 @@
+from schemes.base import BaseScheme
+
+
+class FileScheme(BaseScheme):
+    file_name: str
+    file_type: str

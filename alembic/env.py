b0bc7f580c7d9aa9a1cd55bcdcdb68f8d7fcfcdd
umarr <umarr.developer@gmail.com>
2025-05-18 15:14:04 +0600

    added alembic_postgresql_enum to project

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-
+import alembic_postgresql_enum
 from models import Base
 
 # this is the Alembic Config object, which provides

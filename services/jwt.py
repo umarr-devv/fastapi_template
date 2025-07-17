b8709043c5d678d125ff820df20dda64c8b7dad8
umarr <umarr.developer@gmail.com>
2025-07-17 20:44:28 +0600

    fixed invalid jwt_private_key

--- services/jwt.py
+++ services/jwt.py
@@ -15,8 +15,8 @@ class JWTService:
     ) -> str:
         to_encode = payload.copy()
         to_encode.update({"exp": datetime.now(timezone.utc) + expires_delta})
-        return jwt.encode(to_encode, key=config.jwt_private_key, algorithm=JWTService.algorithm)
+        return jwt.encode(to_encode, key=config.jwt.private_key, algorithm=JWTService.algorithm)
 
     @staticmethod
     def decode(data: str) -> dict:
-        return jwt.decode(data, key=config.jwt_private_key, algorithms=[JWTService.algorithm])
+        return jwt.decode(data, key=config.jwt.private_key, algorithms=[JWTService.algorithm])

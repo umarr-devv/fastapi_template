ffd3ed98a6853ffaf5ef11203d91387fd126256f
umarr <umarr.developer@gmail.com>
2025-08-14 11:14:32 +0600

    гзвфеу get_user dependency

--- deps/user.py
+++ deps/user.py
@@ -11,38 +11,31 @@ from services import JWTService
 
 
 async def get_user(
-        authorization: Annotated[str, Header(...)],
-        rep_manager: Annotated[RepositoryManager, Depends(get_rep_manager)]
+    jwt_token: Annotated[str, Header(...)],
+    rep_manager: Annotated[RepositoryManager, Depends(get_rep_manager)],
 ) -> User:
-    if not authorization:
+    if not jwt_token:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail='missing token for authorization'
+            detail="missing token for authorization",
         )
-    if authorization.startswith('Bearer'):
-        _, token = authorization.split(' ')
-        try:
-            data = JWTService.decode(token)
-            id_, exp = data['sub'], data['exp']
 
-            user = await rep_manager.by_id(User, id_=int(id_))
-            if user:
-                return user
-        except ExpiredSignatureError:
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail='token is expired'
-            )
-        except InvalidSignatureError:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail='invalid token'
-            )
-        except Exception as exc:
-            logging.warning(exc)
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST
-            )
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED
-    )
+    try:
+        data = JWTService.decode(jwt_token)
+        id_, exp = data["sub"], data["exp"]
+
+        user = await rep_manager.by_id(User, id_=int(id_))
+        if user:
+            return user
+    except ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="token is expired"
+        )
+    except InvalidSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="invalid token"
+        )
+    except Exception as exc:
+        logging.warning(exc)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
